0d1ce0fa4a1dd50e9f475caafd7b8938b2c4bec2
Amir <amirsaam@me.com>
2024-07-20 00:53:30 +0330

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -21,16 +21,7 @@ let package = Package(
         .target(
             name: "RadixUI-Swift",
             resources: [
-                .process("RadixIcons/Icons/Abstract.xcassets"),
-                .process("RadixIcons/Icons/Alignment.xcassets"),
-                .process("RadixIcons/Icons/Arrows.xcassets"),
-                .process("RadixIcons/Icons/Borders-Corners.xcassets"),
-                .process("RadixIcons/Icons/Components.xcassets"),
-                .process("RadixIcons/Icons/Design.xcassets"),
-                .process("RadixIcons/Icons/Logos.xcassets"),
-                .process("RadixIcons/Icons/Music.xcassets"),
-                .process("RadixIcons/Icons/Objects.xcassets"),
-                .process("RadixIcons/Icons/Typography.xcassets")
+                .process("RadixIcons/Icons"),
             ]
         ),
         .testTarget(
