5ab89aad2f63ba2075570baaeae7720bacbf5818
amirsaam <amirkamangar@icloud.com>
2024-07-20 03:19:05 +0330

    Register a accessible Bundle name

---
+++ Sources/RadixUI-Swift/RadixIcons/RadixIcons.swift
@@ -0,0 +1,26 @@
+//
+//  File.swift
+//  
+//
+//  Created by Amir Mohammadi on 4/30/1403 AP.
+//
+
+import Foundation
+
+// MARK: - Register RadixIcons Catalog
+extension Bundle {
+    static let radixUIBundle: Bundle = {
+        // The actual bundle name observed in the build output
+        let bundleName = "RadixUI-Swift_RadixUI-Swift"
+
+        // Construct the URL for the bundle
+        guard let bundleURL = Bundle.main.bundleURL
+            .appendingPathComponent(
+                bundleName + ".bundle"
+            ) as URL? else { return Bundle.main }
+
+        // Load the bundle
+        return Bundle(url: bundleURL) ?? Bundle.main
+    }()
+}
+
