f097edb0f265094bff1ad7704f746e2a2f8f76b9
amirsaam <amirkamangar@icloud.com>
2024-07-20 05:22:16 +0330

    Revert "Make Icons correctly resizable via `SVGView`"
    This reverts commit 467b2de5092e3234b7050ffb22f2a27efa2186a4.

--- Sources/RadixUI-Swift/RadixIcons/RadixIcons.swift
+++ Sources/RadixUI-Swift/RadixIcons/RadixIcons.swift
@@ -7,7 +7,6 @@
 
 import Foundation
 import SwiftUI
-import SVGView
 
 // MARK: - Make RadixUI assets available to client app
 public extension Bundle {
@@ -26,18 +25,14 @@ public extension Bundle {
     }()
 }
 
-// MARK: - SwiftUI Extensions
-
-/// Make `Label` accept a `bundle` input just like how `Image` does but use `SVGView` instead of it.
-public extension Label where Title == Text, Icon == SVGView {
-    init(_ title: LocalizedStringKey, svgName: String, bundle: Bundle, size: CGFloat) {
+/// Make `Label` accept a `bundle` input just like how `Image` does
+public extension Label where Title == Text, Icon == Image {
+    init(_ title: LocalizedStringKey, image: String, bundle: Bundle) {
         // Define a closure that returns a Text view
         let titleView: () -> Text = { Text(title) }
         
-        // Define a closure that returns an SVGView view
-        let iconView: () -> SVGView = {
-            return SVGView(contentsOf: bundle.url(forResource: svgName, withExtension: "svg")!)
-        }
+        // Define a closure that returns an Image view
+        let iconView: () -> Image = { Image(image, bundle: bundle) }
         
         // Initialize the Label with the closures
         self.init(title: titleView, icon: iconView)
