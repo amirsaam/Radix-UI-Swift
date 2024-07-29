4a905da810827712e6737e6219e06a1e649efc54
amirsaam <amirkamangar@icloud.com>
2024-07-29 19:29:36 +0330

    Create RadixSegmentedPicker.swift

---
+++ Sources/RadixUI/RadixPrimitives/RadixSegmentedPicker.swift
@@ -0,0 +1,23 @@
+//
+//  RadixSegmentedPicker.swift
+//
+//
+//  Created by Amir Mohammadi on 5/8/1403 AP.
+//
+
+import SwiftUI
+
+struct RadixSegmentedPicker {
+  let backgroundColor: Color
+  let selectedColor: Color
+  let foregroundColor: Color
+  @discardableResult init(backgroundColor: Color, selectedColor: Color, foregroundColor: Color) {
+    self.backgroundColor = backgroundColor
+    self.selectedColor = selectedColor
+    self.foregroundColor = foregroundColor
+    UISegmentedControl.appearance().backgroundColor = UIColor(backgroundColor)
+    UISegmentedControl.appearance().setDividerImage(UIImage(), forLeftSegmentState: .normal, rightSegmentState: .normal, barMetrics: .default)
+    UISegmentedControl.appearance().selectedSegmentTintColor = UIColor(selectedColor)
+    UISegmentedControl.appearance().setTitleTextAttributes([.foregroundColor: UIColor(foregroundColor)], for: .selected)
+  }
+}
