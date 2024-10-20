99f8d64746a14feff5e32c8a9700ffa4bf591e6d
amirsaam <amirkamangar@icloud.com>
2024-10-20 04:00:00 +0330

    Update RadixButton.swift

--- Sources/RadixUI/RadixThemes/RadixButton.swift
+++ Sources/RadixUI/RadixThemes/RadixButton.swift
@@ -8,13 +8,11 @@
 import SwiftUI
 
 fileprivate struct RadixButton: ButtonStyle {
-    func makeBody(configuration: Configuration) -> some View {
-        Group {
-            configuration.label
-        }
-        .scaleEffect(configuration.isPressed ? 0.97 : 1)
-        .opacity(configuration.isPressed ? 0.8 : 1)
-        .animation(.easeInOut(duration: 0.1), value: configuration.isPressed)
+    func makeBody(configuration: Self.Configuration) -> some View {
+        configuration.label
+            .scaleEffect(configuration.isPressed ? 0.97 : 1)
+            .opacity(configuration.isPressed ? 0.8 : 1)
+            .animation(.easeInOut(duration: 0.1), value: configuration.isPressed)
     }
 }
 
